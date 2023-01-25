61cb6599121ed7e1004ba65ecdd0fe2ac2f8698c
Marvin Aulenbacher <marvin.aulenbacher@cariad.technology>
2023-01-25 09:42:50 +0100

    created security groups

--- providers.tf
+++ providers.tf
@@ -1,11 +1,12 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
+  profile = "default"
   region  = "eu-central-1"
 }
